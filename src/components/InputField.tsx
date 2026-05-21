148f072b10f105e28fabdc535be8b34984ff953f
Hassan Qureshi <mhassanqureshi.92@gmail.com>
2026-05-21 15:48:00 +0500

    feat: implement input sanitization patterns in InputField

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-export const InputField: React.FC<any> = ({ label, id, value, onChange }) => {
+export const InputField: React.FC<any> = ({ label, id, value, onChange, inputMode }) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let val = e.target.value.replace(/,/g, '.');
+    if (inputMode === 'decimal') {
+      val = val.replace(/[^0-9.]/g, '');
+    }
+    onChange(val);
+  };
+
   return (
     <div className="input-field-wrapper">
       <label htmlFor={id}>{label}</label>
-      <input id={id} value={value} onChange={(e) => onChange(e.target.value)} />
+      <input id={id} value={value} onChange={handleChange} />
     </div>
   );
 };
