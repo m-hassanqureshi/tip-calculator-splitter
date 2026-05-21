6da5346fe6b69f6059ee943ed578a3396608ebb1
Hassan Qureshi <mhassanqureshi.92@gmail.com>
2026-05-21 18:00:00 +0500

    feat: render display output panel inside App container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { useCalculator } from './hooks/useCalculator';
+import { useCalculator, CURRENCIES } from './hooks/useCalculator';
 import InputField from './components/InputField';
 import TipPresetGrid from './components/TipPresetGrid';
+import DisplayPanel from './components/DisplayPanel';
 import './App.css';
 
 export const App: React.FC = () => {
-  const { state, errors, setBill, setTipPercent, setPeople } = useCalculator();
+  const { state, errors, outputs, isResetEnabled, setBill, setTipPercent, setPeople, reset } = useCalculator();
+  const currencyConfig = CURRENCIES[state.currency];
   return (
     <div className="app-container">
       <header className="app-header">
@@ -16,6 +18,9 @@ export const App: React.FC = () => {
           <InputField id="bill" label="Bill" value={state.bill} onChange={setBill} />
           <TipPresetGrid value={state.tipPercent} onChange={setTipPercent} />
           <InputField id="people" label="People" value={state.people} onChange={setPeople} />
+        </section>
+        <section className="outputs-panel">
+          <DisplayPanel currency={currencyConfig} {...outputs} isResetEnabled={isResetEnabled} onReset={reset} />
         </section>
       </main>
     </div>
