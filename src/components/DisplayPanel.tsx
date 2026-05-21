c724c09d013681b1748b4e4b466c2045212ddc60
Hassan Qureshi <mhassanqureshi.92@gmail.com>
2026-05-21 17:00:00 +0500

    feat: implement dynamic font size scale logic in DisplayPanel

--- src/components/DisplayPanel.tsx
+++ src/components/DisplayPanel.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 export const DisplayPanel: React.FC<any> = ({ totalTip, grandTotal }) => {
   const formatAmount = (val: number) => val.toFixed(2);
+  const getFontSize = (amount: number) => amount > 100000 ? 'small' : 'large';
   return (
     <div className="display-panel">
-      <div>Total Tip: {formatAmount(totalTip)}</div>
-      <div>Grand Total: {formatAmount(grandTotal)}</div>
+      <div className={getFontSize(totalTip)}>Total Tip: {formatAmount(totalTip)}</div>
+      <div className={getFontSize(grandTotal)}>Grand Total: {formatAmount(grandTotal)}</div>
     </div>
   );
 };
